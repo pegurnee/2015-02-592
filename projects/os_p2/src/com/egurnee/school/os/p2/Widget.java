bbd24a66cac689cef3179002959407c9b2232638
Eddie Gurnee <pegurnee@gmail.com>
2015-05-30 21:00:14 -0400

    added some widget code

--- projects/os_p2/src/com/egurnee/school/os/p2/Widget.java
+++ projects/os_p2/src/com/egurnee/school/os/p2/Widget.java
@@ -1,5 +1,17 @@
 package com.egurnee.school.os.p2;
 
 public class Widget {
+	private int numberTouched;
 
+	public Widget() {
+		this.numberTouched = 0;
+	}
+
+	public int getWorked() {
+		return this.numberTouched;
+	}
+
+	public void workUpon() {
+		this.numberTouched++;
+	}
 }
