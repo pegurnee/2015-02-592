b2d31c5abd11686468f1b7026564a76fc0ea1b76
Eddie Gurnee <pegurnee@gmail.com>
2015-06-18 19:34:36 -0400

    implemented pagesequence and factory

--- projects/os_p5/src/com/egurnee/school/os/p5/PageSeq.java
+++ projects/os_p5/src/com/egurnee/school/os/p5/PageSeq.java
@@ -1,5 +1,27 @@
 package com.egurnee.school.os.p5;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class PageSeq {
 
+	private final int numFramesOfMemory;
+	private final LinkedList<Integer> pageRequests;
+
+	public PageSeq(int numFramesOfMemory) {
+		this.numFramesOfMemory = numFramesOfMemory;
+		this.pageRequests = new LinkedList<Integer>();
+	}
+
+	public void addPageRequest(int nextInt) {
+		this.pageRequests.add(nextInt);
+	}
+
+	public int getFramesOfMemory() {
+		return this.numFramesOfMemory;
+	}
+
+	public Iterator<Integer> getSequenceIterator() {
+		return this.pageRequests.iterator();
+	}
 }
---
+++ projects/os_p5/src/com/egurnee/school/os/p5/PageSequenceFactory.java
@@ -0,0 +1,43 @@
+package com.egurnee.school.os.p5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PageSequenceFactory {
+	public static ArrayList<PageSeq> getTriesFromFile(String filename) {
+		ArrayList<PageSeq> tries = new ArrayList<>();
+		Scanner input = null;
+
+		try {
+			input = new Scanner(new FileInputStream(filename));
+
+			while (input.hasNextInt()) {
+				tries.add(PageSequenceFactory.getSequence(input));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} finally {
+			input.close();
+		}
+
+		return tries;
+	}
+
+	private static PageSeq getSequence(Scanner input) {
+		final int numFramesOfMemory = input.nextInt();
+		PageSeq theSequence = new PageSeq(numFramesOfMemory);
+
+		while (input.hasNextInt()) {
+			final int nextInt = input.nextInt();
+			if (nextInt == -1) {
+				break;
+			}
+			theSequence.addPageRequest(nextInt);
+		}
+		return theSequence;
+
+	}
+}
