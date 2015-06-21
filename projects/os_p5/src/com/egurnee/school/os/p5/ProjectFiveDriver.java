6d77b5b42efe3a2f8608627d1833f08de1db41b5
Eddie Gurnee <pegurnee@gmail.com>
2015-06-20 22:19:03 -0400

    added the p5 driver

--- projects/os_p5/src/com/egurnee/school/os/p5/ProjectFiveDriver.java
+++ projects/os_p5/src/com/egurnee/school/os/p5/ProjectFiveDriver.java
@@ -6,8 +6,10 @@ public class ProjectFiveDriver {
 	public static void main(String[] args) {
 		final ArrayList<PageSeq> triesFromFile = PageSequenceFactory
 				.getTriesFromFile("sample_input.txt");
+		final ArrayList<PageSeq> triesFromFile2 = PageSequenceFactory
+				.getTriesFromFile("homework_input.txt");
 
-		final PagingManager pagingManager = new PagingManager(triesFromFile);
+		final PagingManager pagingManager = new PagingManager(triesFromFile2);
 
 		// pagingManager.printAll(4);
 	}
