d4bd1ff530ccb07e1a8a8d31b760016084f227d8
Eddie Gurnee <pegurnee@gmail.com>
2015-06-25 13:55:22 -0400

    completed DiskRequest

---
+++ projects/os_p6/src/os_p6/DiskAllocationException.java
@@ -0,0 +1,30 @@
+package os_p6;
+
+public class DiskAllocationException extends Exception {
+
+	public DiskAllocationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiskAllocationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiskAllocationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiskAllocationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiskAllocationException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- projects/os_p6/src/os_p6/DiskRequest.java
+++ projects/os_p6/src/os_p6/DiskRequest.java
@@ -7,7 +7,7 @@ public class DiskRequest {
 		ADD, APPEND, DELETE, PRINT, READ;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DiskAllocationException {
 		new DiskRequest("print");
 		new DiskRequest("read \"fourth in line.dat\"");
 		new DiskRequest("add \"second hand.txt\" 8");
@@ -18,32 +18,63 @@ public class DiskRequest {
 
 	private RequestType type;
 
-	public DiskRequest(String requestLine) {
+	public DiskRequest(String requestLine) throws DiskAllocationException {
 		String[] tokens = Arrays.stream(requestLine.split("\""))
 				.map(x -> x.trim()).toArray(size -> new String[size]);
 		switch (tokens[0]) {
 			case "add":
-				System.out.println(tokens[2]);
+				this.type = RequestType.ADD;
 				break;
 			case "append":
-
+				this.type = RequestType.APPEND;
 				break;
 			case "del":
 			case "delete":
-
+				this.type = RequestType.DELETE;
 				break;
 			case "read":
-				System.out.println(tokens[1]);
+				this.type = RequestType.READ;
 				break;
 			case "print":
-				System.out.println(tokens[0]);
+				this.type = RequestType.PRINT;
 				break;
-
 			default:
-				// for (String string : tokens) {
-				// System.out.println(string);
-				// }
-				break;
+				throw new DiskAllocationException();
 		}
+		if (!"print".equals(tokens[0])) {
+			this.filename = tokens[1];
+			if ("add".equals(tokens[0]) || "append".equals(tokens[0])) {
+				this.size = Integer.parseInt(tokens[2]);
+			}
+		}
+	}
+
+	/**
+	 * @return the filename
+	 * @throws DiskAllocationException
+	 */
+	public final String getFilename() throws DiskAllocationException {
+		if (this.filename == null) {
+			throw new DiskAllocationException();
+		}
+		return this.filename;
+	}
+
+	/**
+	 * @return the size
+	 * @throws DiskAllocationException
+	 */
+	public final int getSize() throws DiskAllocationException {
+		if (!(this.size > 0)) {
+			throw new DiskAllocationException();
+		}
+		return this.size;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public final RequestType getType() {
+		return this.type;
 	}
 }
